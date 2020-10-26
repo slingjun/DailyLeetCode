d633af36c7e862053c0d049261e01f3e8d679aa3
IMLaoBa <73049845+SxxyOldEight@users.noreply.github.com>
2020-10-26 02:18:20 -0400

    Update Day2.cpp
    update

--- Day2.cpp
+++ Day2.cpp
@@ -10,12 +10,12 @@ public:
     
     bool dfs(vector<int>& nums, int size, int index, int sum, int target, int k){
         // k == 4 in this problem
-        if(k == 0) return true;
+        if(k == 1) return true;
         if(target < 0 || index >= size) return false;
         bool result = false;
-        for(int i = index+1; i < size; ++i){
-            result = (target == 0) ? (result||dfs(nums, size, index+1, sum, sum, k-1))  
-            : (result||dfs(nums, size, index+1, sum, target-nums[index], k));
+        for(int i = index; i < size; ++i){
+            result = (target == 0) ? (result||dfs(nums, size, i+1, sum, sum, k-1))  
+            : (result||dfs(nums, size, i+1, sum, target-nums[i], k));
         }
         return result;
     }
