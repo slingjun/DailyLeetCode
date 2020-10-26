cbe08f42a05eee89fc0a61292944e02f1daa63b5
IMLaoBa <73049845+SxxyOldEight@users.noreply.github.com>
2020-10-26 14:41:56 -0400

    Update Day2.cpp
    update

--- Day2.cpp
+++ Day2.cpp
@@ -2,21 +2,25 @@ class Solution {
 public: 
     //equivalent to the problem: partitioning the array into 4 groups with equal sum
     bool makesquare(vector<int>& nums) {
+        if(nums.size() == 0) return false;
         int target = accumulate(nums.begin(), nums.end(), 0);
-        if(target % 2 != 0) return false;
-        target /= 4;
-        return dfs(nums, (int)nums.size(), 0, target, target, 4);
+        if (target % 4 != 0) return false;
+        return dfs(nums,0,target/4, target/4, 4);
     }
     
-    bool dfs(vector<int>& nums, int size, int index, int sum, int target, int k){
+    bool dfs(vector<int>& nums, int index, int sum, int target, int k){
         // k == 4 in this problem
-        if(k == 1) return true;
-        if(target < 0 || index >= size) return false;
-        bool result = false;
-        for(int i = index; i < size; ++i){
-            result = (target == 0) ? (result||dfs(nums, size, i+1, sum, sum, k-1))  
-            : (result||dfs(nums, size, i+1, sum, target-nums[i], k));
+        if(k == 0) return true;
+        if(target < 0) return false;
+        if(target == 0) return dfs(nums, 0, sum, sum, k-1);
+        for(int i = index; i < nums.size(); ++i){
+            if(nums[i]!=-2){
+                int tmp = nums[i];
+                nums[i] = -2;
+                if(dfs(nums, i+1,sum,target-tmp, k)) return true;
+                nums[i] = tmp;
+            }
         }
-        return result;
+        return false;
     }
 };
