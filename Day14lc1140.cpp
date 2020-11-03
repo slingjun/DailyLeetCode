1226a2ee3c108be21b337aaa3e46a7f6c6bd5977
IMLaoBa <73049845+SxxyOldEight@users.noreply.github.com>
2020-11-03 03:02:15 -0500

    Update Day14lc1140.cpp

--- Day14lc1140.cpp
+++ Day14lc1140.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    int memo[101][101][2];
-    int dp(int i,int p,int m,vector<int>& stones)
-    {
-        if(i>=stones.size())
-            return 0;
-        else if(memo[i][m][p]!=0)
-            return memo[i][m][p];
-        else
-        {
-            int val=0,q=(p==0?INT_MIN:INT_MAX),temp=0;
-            for(int j=1;j<=2*m&&i+j<=stones.size();++j)
-            {
-                val+=stones[i+j-1];
-                if(p==0)
-                    q=max(q,val+dp(i+j,1,max(j,m),stones));
-                else
-                    q=min(q,dp(i+j,0,max(j,m),stones));
+    int stoneGameII(vector<int>& piles) {
+        return dp(piles, 0, 1, 0);
+    }
+    
+    int dp(vector<int> &piles, int start, int m, int player){//player0: me; player1: other
+
+        if(start >=piles.size()) return 0;
+        if(memo[start][m][player]!=0) return memo[start][m][player];
+        int end = start+2*m;
+        int accumulate = 0; // a running total
+        int tmp = (player == 0) ? INT_MIN : INT_MAX;
+        for(int i = start; i < end && i < piles.size(); ++i){
+            accumulate += piles[i]; //record the stone amount before the next start
+            if(player == 0){
+                tmp = max(tmp, accumulate+dp(piles, i+1, max(i-start+1, m), 1)); // note i-start+1 is the X where 1<=X<=2m
+            }else{
+                tmp = min(tmp, dp(piles, i+1, max(i-start+1, m), 0)); //player 2 wants to minimize my stone count
             }
-            return memo[i][m][p]=q;
         }
+        memo[start][m][player] = tmp;
+        return tmp;
     }
-    int stoneGameII(vector<int>& piles)
-	{
-        return dp(0,0,1,piles);
-    }
+private:
+    int memo[101][101][2];
 };
